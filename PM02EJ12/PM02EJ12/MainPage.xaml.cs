5b6250075aec8cce9309c42aa55ea10c84ab00e0
Roberto Mejía <robenriquemejia@gmail.com>
2022-02-11 23:05:21 -0600

    Add project files.

---
+++ PM02EJ12/PM02EJ12/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PM02EJ12
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Enviar(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(name.Text) && !string.IsNullOrEmpty(lname.Text) && !string.IsNullOrEmpty(age.Text) && !string.IsNullOrEmpty(email.Text))
+            {
+                var Persona = new Models.Persona
+                {
+                    name = "Nombre: " + this.name.Text,
+                    lname = "Apellido: " + this.lname.Text,
+                    age = "Edad: " + this.age.Text,
+                    email = "Correo: " + this.email.Text
+                };
+
+                PersonData pdata = new PersonData();
+                pdata.BindingContext = Persona;
+                await Navigation.PushAsync(pdata);
+            }
+            else
+                DisplayAlert("Error", "Debe llenar todos los campos", "OK");
+        }
+    }
+}
---
+++ PM02EJ12/PM02EJ12/Models/Persona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM02EJ12.Models
+{
+    internal class Persona
+    {
+        public string name { get; set; }
+        public string lname { get; set; }
+        public string age { get; set; }
+        public string email { get; set; }
+    }
+}
